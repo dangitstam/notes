113f2f7f76f93076e6eab2cc2c6a0d757d08ebd0
Tam Dang <tamdangnadmat@gmail.com>
2020-12-22 15:32:36 -0800

    new routes within app: list view and create view

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Notes',
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Notes')),
-            body: CoffeeTastingListViewWidget()));
+    return MaterialApp(title: 'Notes', initialRoute: '/', routes: {
+      '/': (context) => CoffeeTastingListViewScreen(),
+      '/second': (context) => CoffeeTastingCreateViewScreen(),
+    });
+  }
+}
+
+class CoffeeTastingListViewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Notes'),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: RaisedButton(
+                color: Colors.lightBlue,
+                onPressed: () {
+                  // Navigate to the second screen using a named route.
+                  Navigator.pushNamed(context, '/second');
+                },
+                child: Text('New', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
+        body: CoffeeTastingListViewWidget());
+  }
+}
+
+class CoffeeTastingCreateViewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Notes'),
+          leading: GestureDetector(
+            onTap: () {
+              // Navigate to the second screen using a named route.
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back, // add custom icons also
+            ),
+          ),
+        ),
+        body: Center(child: Text('Hello, Create View!')));
   }
 }
