d9d2d08c0775bfcf4e0b26f73792efcde21689ef
Tam Dang <tamdangnadmat@gmail.com>
2021-01-02 23:20:29 -0800

    camel case from snake case

--- lib/src/data/app_database.dart
+++ lib/src/data/app_database.dart
@@ -60,12 +60,12 @@ Future<void> _createCoffeeTastingsTable(Database db) {
   ).then((_) async {
     // For development purposes, populate the database from the
     // coffee_tastings.json asset.
-    var coffee_tastings_string = await rootBundle.loadString('assets/coffee_tastings.json');
+    var coffeeTastingsString = await rootBundle.loadString('assets/coffee_tastings.json');
 
     // Update stream so that the downstream list view is updated.
-    List<dynamic> coffee_tastings = json.decode(coffee_tastings_string);
+    List<dynamic> coffeeTastings = json.decode(coffeeTastingsString);
     var coffeeTastingBloc = CoffeeTastingRepository();
-    for (var coffeeTasting in coffee_tastings) {
+    for (var coffeeTasting in coffeeTastings) {
       await coffeeTastingBloc.insert(CoffeeTasting.fromAppDatabase(coffeeTasting));
     }
   });
@@ -103,14 +103,14 @@ Future<void> _createCoffeeTastingNotesTable(Database db) {
     """,
   ).then((_) async {
     // For development purposes, populate the database from the notes.json asset.
-    var coffee_tasting_notes_string = await rootBundle.loadString('assets/coffee_tastings_notes.json');
+    var coffeeTastingNotesString = await rootBundle.loadString('assets/coffee_tastings_notes.json');
 
     // Update stream so that the downstream list view is updated.
     var coffeeTastingDao = CoffeeTastingNoteDao(database: AppDatabase.db.database);
 
-    List<dynamic> coffee_tasting_notes = json.decode(coffee_tasting_notes_string);
-    coffee_tasting_notes.forEach((coffee_tasting_note) {
-      coffeeTastingDao.insert(coffee_tasting_note);
+    List<dynamic> coffeeTastingNotes = json.decode(coffeeTastingNotesString);
+    coffeeTastingNotes.forEach((coffeeTastingNote) {
+      coffeeTastingDao.insert(coffeeTastingNote);
     });
   });
 }
