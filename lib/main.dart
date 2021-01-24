4492b259ec71f54a1fcacf3d5b745f233209a1fe
Tam Dang <tamdangnadmat@gmail.com>
2021-01-23 17:36:02 -0800

    rename to light theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes/src/coffee_tasting_create_view/bloc/coffee_tasting_create_bloc.dart';
-import 'package:notes/src/styles/typography.dart';
+import 'package:notes/src/styles/light_theme.dart';
 
 import 'src/coffee_tasting_create_view/coffee_tasting_create_view.dart';
 import 'src/coffee_tasting_list_view/coffee_tasting_list_view.dart';
