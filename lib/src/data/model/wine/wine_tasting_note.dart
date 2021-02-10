2f783b46c922f2a3fc7d416f10b0ed5826a740de
Tam Dang <tamdangnadmat@gmail.com>
2021-02-09 20:53:57 -0800

    fix WineTastingNote parameter names

--- lib/src/data/model/wine/wine_tasting_note.dart
+++ lib/src/data/model/wine/wine_tasting_note.dart
@@ -1,19 +1,19 @@
 class WineTastingNote {
-  final int coffee_tasting_id;
+  final int wine_tasting_id;
   final int note_id;
 
-  WineTastingNote({this.coffee_tasting_id, this.note_id});
+  WineTastingNote({this.wine_tasting_id, this.note_id});
 
-  factory WineTastingNote.fromAppDatabase(Map<String, dynamic> coffeeTastingNoteMap) {
+  factory WineTastingNote.fromAppDatabase(Map<String, dynamic> wineTastingNoteMap) {
     return WineTastingNote(
-      coffee_tasting_id: coffeeTastingNoteMap['coffee_tasting_id'],
-      note_id: coffeeTastingNoteMap['note_id'],
+      wine_tasting_id: wineTastingNoteMap['wine_tasting_id'],
+      note_id: wineTastingNoteMap['note_id'],
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'coffee_tasting_id': coffee_tasting_id,
+      'wine_tasting_id': wine_tasting_id,
       'note_id': note_id,
     };
   }
