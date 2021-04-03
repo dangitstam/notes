df3055a810c5a65430f5f21d0e0c17c97949d506
Tam Dang <tamdangnadmat@gmail.com>
2021-04-03 11:13:20 -0700

    new tap approach for characteristics

--- lib/src/features/wine_tasting_create_view/components/characteristics/wine_characteristics_section.dart
+++ lib/src/features/wine_tasting_create_view/components/characteristics/wine_characteristics_section.dart
@@ -2,13 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-import 'package:notes/src/common/util.dart';
 import 'package:notes/src/features/wine_tasting_create_view/bloc/wine_tasting_create_bloc.dart';
-import 'package:notes/src/features/wine_tasting_create_view/components/characteristics/characteristics_chart.dart';
-import 'package:notes/src/features/wine_tasting_create_view/components/characteristics/swiper_tabs.dart';
 
 import '../section_title.dart';
-import 'characteristics_sliders.dart';
 
 class WineCharacteristicsScreen extends StatefulWidget {
   @override
@@ -72,135 +68,228 @@ class _CharacteristicsSectionState extends State<CharacteristicsSection> {
   Widget build(BuildContext context) {
     var wineTastingState = context.watch<WineTastingCreateBloc>().state.tasting;
 
-    /**
-     * Pair of sliders for each characteristic.
-     */
-    var swiperWidgets = [
-      CharacteristicsSliders(
-        characteristic: 'Aroma',
-        score: wineTastingState.aromaScore,
-        intensity: wineTastingState.aromaIntensity,
-        updateScore: (value) {
-          context.read<WineTastingCreateBloc>().add(AromaScoreEvent(aromaScore: round(value)));
+    var characteristics = [
+      {
+        'name': 'Acidity',
+        'value': wineTastingState.acidityIntensity,
+        'on_changed': (value) {
+          context.read<WineTastingCreateBloc>().add(AcidityIntensityEvent(acidityIntensity: value.toDouble()));
         },
-        updateIntensity: (value) {
-          context.read<WineTastingCreateBloc>().add(AromaIntensityEvent(aromaIntensity: round(value)));
+      },
+      {
+        'name': 'Sweetness',
+        'value': wineTastingState.sweetnessIntensity,
+        'on_changed': (value) {
+          context.read<WineTastingCreateBloc>().add(SweetnessIntensityEvent(sweetnessIntensity: value.toDouble()));
         },
-      ),
-      CharacteristicsSliders(
-        characteristic: 'Acidity',
-        score: wineTastingState.acidityScore,
-        intensity: wineTastingState.acidityIntensity,
-        updateScore: (value) {
-          context.read<WineTastingCreateBloc>().add(AcidityScoreEvent(acidityScore: round(value)));
+      },
+      {
+        'name': 'Astringency',
+        'value': wineTastingState.acidityIntensity,
+        'on_changed': (value) {
+          context.read<WineTastingCreateBloc>().add(AcidityIntensityEvent(acidityIntensity: value.toDouble()));
         },
-        updateIntensity: (value) {
-          context.read<WineTastingCreateBloc>().add(AcidityIntensityEvent(acidityIntensity: round(value)));
+      },
+      {
+        'name': 'Body',
+        'value': wineTastingState.bodyLevel,
+        'on_changed': (value) {
+          context.read<WineTastingCreateBloc>().add(BodyLevelEvent(bodyLevel: value.toDouble()));
         },
-      ),
-      CharacteristicsSliders(
-        characteristic: 'Body',
-        score: wineTastingState.bodyScore,
-        intensity: wineTastingState.bodyLevel,
-        intensitySliderLabel: 'Level',
-        intensityPositiveEndLabel: Text(
-          'Heavy',
-          style: Theme.of(context).textTheme.caption.copyWith(fontWeight: FontWeight.bold),
-        ),
-        intensityNegativeEndLabel: Text(
-          'Thin',
-          style: Theme.of(context).textTheme.caption.copyWith(fontWeight: FontWeight.bold),
-        ),
-        updateScore: (value) {
-          context.read<WineTastingCreateBloc>().add(BodyScoreEvent(bodyScore: round(value)));
-        },
-        updateIntensity: (value) {
-          context.read<WineTastingCreateBloc>().add(BodyLevelEvent(bodyLevel: round(value)));
-        },
-      ),
-      CharacteristicsSliders(
-        characteristic: 'Sweetness',
-        score: wineTastingState.sweetnessScore,
-        intensity: wineTastingState.sweetnessIntensity,
-        updateScore: (value) {
-          context.read<WineTastingCreateBloc>().add(SweetnessScoreEvent(sweetnessScore: round(value)));
-        },
-        updateIntensity: (value) {
-          context.read<WineTastingCreateBloc>().add(SweetnessIntensityEvent(sweetnessIntensity: round(value)));
-        },
-      ),
-      CharacteristicsSliders(
-        characteristic: 'Finish',
-        score: wineTastingState.finishScore,
-        intensity: wineTastingState.finishDuration,
-        intensitySliderLabel: 'Duration',
-        intensityPositiveEndLabel: Text(
-          'Long',
-          style: Theme.of(context).textTheme.caption.copyWith(fontWeight: FontWeight.bold),
-        ),
-        intensityNegativeEndLabel: Text(
-          'Short',
-          style: Theme.of(context).textTheme.caption.copyWith(fontWeight: FontWeight.bold),
-        ),
-        updateScore: (value) {
-          context.read<WineTastingCreateBloc>().add(FinishScoreEvent(finishScore: round(value)));
-        },
-        updateIntensity: (value) {
-          context.read<WineTastingCreateBloc>().add(FinishDurationEvent(finishDuration: round(value)));
-        },
-      ),
+      }
     ];
 
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SectionTitle(sectionNumber: 2, title: 'Characteristics'),
           SizedBox(height: 20),
-          Text(
-            'Identify & assess characteristics.',
-            style: Theme.of(context).textTheme.caption,
-          ),
-          SizedBox(height: 20),
-          /**
-           * Chart displaying characteristic scores and intensities.
-           */
-          CharacteristicsChart(
-            tasting: context.watch<WineTastingCreateBloc>().state.tasting,
-          ),
-          SizedBox(height: 20),
-          /**
-           * Swipeable slider section for editing characteristics.
-           */
-          Expanded(
-            child: Swiper(
-              itemCount: swiperWidgets.length,
-              itemBuilder: (BuildContext context, int index) {
-                return swiperWidgets[index];
-              },
-              control: SwiperControl(
-                color: Theme.of(context).colorScheme.onSurface,
-                iconNext: CupertinoIcons.chevron_right,
-                iconPrevious: CupertinoIcons.chevron_left,
-                padding: const EdgeInsets.all(0.0),
-                size: 25,
-              ),
-              controller: swiperController,
-              onIndexChanged: (index) => {onSwiperToggleButtonClick(index)},
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Tap any notch to note the intensity of a characteristic on a scale of zero to six with ',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                WidgetSpan(
+                  child: Icon(
+                    CupertinoIcons.xmark_circle,
+                    size: 16,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                TextSpan(
+                  text: '.',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              ],
             ),
           ),
-          SizedBox(height: 20),
-          SwiperTabs(
-            swiperTitles: swiperTabsTitles,
-            isSelected: swiperToggleButtonsSelections,
-            onTap: (int index) {
-              swiperController.move(index);
-            },
+          SizedBox(height: 16),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Tap ',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                WidgetSpan(
+                  child: Icon(
+                    CupertinoIcons.xmark_circle,
+                    size: 16,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                TextSpan(
+                  text: ' again to undo the mark at that position.',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 40),
+          SizedBox(height: 30),
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                String name = characteristics[index]['name'];
+                double value = characteristics[index]['value'];
+                var onChanged = characteristics[index]['on_changed'];
+
+                return Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          name.toUpperCase(),
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context).textTheme.overline,
+                        ),
+                        Text('${value} / 6', style: Theme.of(context).textTheme.subtitle2),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    CharacteristicStrengthWidget(
+                      value: value.toInt(),
+                      onChanged: onChanged,
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => Divider(height: 50),
+              itemCount: characteristics.length,
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class CharacteristicStrengthNotch extends StatefulWidget {
+  final int notch_level; // The position of this notch as used in the strength widget.
+  final int current_level; // The current strength level specified by the user.
+  final onTap;
+  CharacteristicStrengthNotch({this.notch_level, this.current_level, this.onTap});
+
+  @override
+  _CharacteristicStrengthNotchState createState() => _CharacteristicStrengthNotchState();
+}
+
+class _CharacteristicStrengthNotchState extends State<CharacteristicStrengthNotch> {
+  @override
+  Widget build(BuildContext context) {
+    var iconData = widget.current_level >= widget.notch_level ? CupertinoIcons.xmark_circle : CupertinoIcons.minus;
+    var color = widget.current_level >= widget.notch_level
+        ? Theme.of(context).colorScheme.primary
+        : Theme.of(context).colorScheme.onSurface;
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Icon(
+          iconData,
+          color: color,
+          size: 30,
+        ),
+      ),
+      onTap: widget.onTap,
+    );
+  }
+}
+
+class CharacteristicStrengthWidget extends StatefulWidget {
+  int value;
+  final Function(int) onChanged;
+  final String weakLabel;
+  final String strongLabel;
+  CharacteristicStrengthWidget({
+    this.value,
+    this.onChanged,
+    this.weakLabel = 'Weak',
+    this.strongLabel = 'Strong',
+  });
+
+  @override
+  _CharacteristicStrengthWidgetState createState() => _CharacteristicStrengthWidgetState();
+}
+
+class _CharacteristicStrengthWidgetState extends State<CharacteristicStrengthWidget> {
+  var _selectedPosition;
+  var _value;
+  final _numNotches = 6;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedPosition = widget.value - 1;
+    _value = widget.value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(
+            _numNotches,
+            (index) => CharacteristicStrengthNotch(
+              notch_level: index,
+              current_level: _selectedPosition,
+              onTap: () {
+                setState(() {
+                  if (_selectedPosition == index) {
+                    _selectedPosition -= 1;
+                  } else {
+                    _selectedPosition = index;
+                  }
+
+                  // _selectedPosition is zero-indexed, bump by one to represent the actual value.
+                  _value = _selectedPosition + 1;
+                  widget.onChanged(_value);
+                });
+              },
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.weakLabel,
+              style: Theme.of(context).textTheme.caption,
+            ),
+            Text(
+              widget.strongLabel,
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
